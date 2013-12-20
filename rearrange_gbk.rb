88e2ed06c15be4c5d1ff96aee2106dd9faa6d528
Josh Earl <joshearl1@hotmail.com>
2013-12-20 02:28:46 -0500

    Made this program actually functional

--- rearrange_gbk.rb
+++ rearrange_gbk.rb
@@ -2,4 +2,22 @@
 
 require 'bio'
 
-ff = Bio::FlatFile.new(Bio::GenBank, ARGF)
+fas_ff = Bio::FlatFile.auto(ARGV[0])
+gbk_ff = Bio::GenBank.open(ARGV[1])
+genbank_hash = {}
+
+gbk_ff.each_entry do |gb|
+  #puts gb.locus.entry_id
+#  genbank_hash[gb.locus.entry_id] = gb
+  genbank_hash[gb.definition] = gb
+end
+
+puts "\n***************\nEntering the fasta file\n****************\n"
+
+fas_ff.each_entry do |f|
+  matching_entry = ''
+  genbank_hash.keys.each_entry do |gk|
+    matching_entry = gk if genbank_hash[gk].definition.match(f.definition)
+  end
+  puts genbank_hash[matching_entry].to_biosequence.output(:genbank)
+end
